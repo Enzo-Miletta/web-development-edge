210555bef3bc3d39c61b511c498fc8f26f71fde3
Enzo Miletta <milettaaa@gmail.com>
2024-03-15 20:57:58 -0300

    a 2 last classrooms from arduino
    .

---
+++ aula3.ino
@@ -0,0 +1,72 @@
+/* aula 03-logica de programação (c e c++)
+ a = 10
+b = 8
+soma = a+b
+saida = 18
+int serve como uma variavel exemplo:
+int media(int n1, int n2, int n3, int n4, int n5){
+int media = 0
+media = (n1+n2+n3+n4+n5)/5
+return media/
+}
+/n é utilizado para quebrar ou pular uma linha
+https://www.programiz.com/ para verificar como o código funcionará[
+ent = &&
+or = || 
+*/
+
+int led = 13;
+void setup()
+{
+  pinMode(led, OUTPUT);
+}
+
+
+void loop()
+{
+digitalWrite(led, HIGH);
+  delay(1000);
+    digitalWrite(led, LOW);
+  delay(500);
+  
+  digitalWrite(led, HIGH);
+  delay(1000);
+    digitalWrite(led, LOW);
+  delay(500);
+  
+  digitalWrite(led, HIGH);
+  delay(1000);
+    digitalWrite(led, LOW);
+  delay(500);
+  
+  
+  digitalWrite(led, HIGH);
+  delay(3000);
+    digitalWrite(led, LOW);
+  delay(500);
+  
+  digitalWrite(led, HIGH);
+  delay(3000);
+    digitalWrite(led, LOW);
+  delay(500);
+   
+  digitalWrite(led, HIGH);
+     delay(3000);
+    digitalWrite(led, LOW);
+  delay(500);
+  
+   digitalWrite(led, HIGH);
+   delay(1000);
+    digitalWrite(led, LOW);
+  delay(500);
+  
+   digitalWrite(led, HIGH);
+   delay(1000);
+    digitalWrite(led, LOW);
+  delay(500);
+  
+   digitalWrite(led, HIGH);
+   delay(1000);
+    digitalWrite(led, LOW);
+  delay(500);
+}
